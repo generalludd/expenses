f9a6a2b188ae011e08c894fd534df488a0b2b1e1
Chris Dart <chrisdart@cerebratorium.com>
2013-12-29 20:09:07 -0600

    Minor refactoring

--- application/views/expense/select_month.php
+++ application/views/expense/select_month.php
@@ -1,15 +1,33 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
 
-// select_month.php Chris Dart Dec 29, 2013 5:04:43 PM chrisdart@cerebratorium.com
-$buttons[] = array("item"=>"expense", "text"=>"Go", "type"=>"span", "class"=>"button go-to-month");
-$buttons[] = array("item" => "expense", "text" => "Current Month", "type" => "span", "class" => "button show-current-month");
+defined('BASEPATH') or exit('No direct script access allowed');
+
+// select_month.php Chris Dart Dec 29, 2013 5:04:43 PM
+// chrisdart@cerebratorium.com
+$buttons[] = array(
+        "item" => "expense",
+        "text" => "Go",
+        "type" => "span",
+        "class" => "button go-to-month"
+);
+$buttons[] = array(
+        "item" => "expense",
+        "text" => "Current Month",
+        "type" => "span",
+        "class" => "button show-current-month"
+);
 
 ?>
 <div id="month-selector">
-<p>
+	<p>
 <?=form_dropdown("search-month",$month_list,$default_month,"id='search-month'");?>
-&nbsp;<input type="text" size="5" maxlength="4" id="search-year"
-		name="search-year" value="<?=$default_year;?>" />
-		</p>
+&nbsp;<input
+			type="text"
+			size="5"
+			maxlength="4"
+			id="search-year"
+			name="search-year"
+			value="<?=$default_year;?>" />
+	</p>
 		<?=create_button_bar($buttons);?>
 		</div>
