095a48f5c1b7cba3726cd7da251345f13e24a589
Chris Dart <chrisdart@cerebratorium.com>
2014-09-15 20:47:24 -0500

    Updating format of login for mobile

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -5,10 +5,10 @@
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 <title>Red House Expense Tracking System</title>
 <link type="text/css" rel="stylesheet" media="all"  href="<?=base_url("/css/main.css");?>" />
-<link type="text/css" rel="stylesheet"  media="(max-width: 855px)" href="<?=base_url("css/mobile.css")?>" />
-<script type="text/javascript" src="<?=base_url();?>js/jquery.min.js"></script>
+<link type="text/css" rel="stylesheet"  media="all" href="<?=base_url("css/mobile.css")?>" />
+<script type="text/javascript" src="<?=base_url("js/jquery.min.js");?>"></script>
 
-<script type="text/javascript" src="<?=base_url();?>/js/password.js"></script>
+<script type="text/javascript" src="<?=base_url("js/password.js");?>"></script>
 </head>
 <body class="not-logged-in">
 <div id="main">
