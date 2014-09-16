adcde2c5684ea3c6917b5ce9fd2c68067a3e48ea
Chris Dart <chrisdart@cerebratorium.com>
2014-09-15 21:25:05 -0500

    More mobile fixing

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -5,9 +5,10 @@
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 <title>Red House Expense Tracking System</title>
 <link type="text/css" rel="stylesheet" media="all"  href="<?=base_url("/css/main.css");?>" />
-<link type="text/css" rel="stylesheet"  media="all" href="<?=base_url("css/mobile.css")?>" />
+<link type="text/css" rel="stylesheet"  media="only screen 
+and (min-device-width : 320px) 
+and (max-device-width : 568px)" href="<?=base_url("css/mobile.css")?>" />
 <script type="text/javascript" src="<?=base_url("js/jquery.min.js");?>"></script>
-
 <script type="text/javascript" src="<?=base_url("js/password.js");?>"></script>
 </head>
 <body class="not-logged-in">
