5ce6ba3e7c50809d4cca91a7ee7a57463f47822b
Chris Dart <chrisdart@cerebratorium.com>
2014-10-17 10:30:10 -0500

    change to viewport size on login screen

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -3,6 +3,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<meta name="viewport"
+    content="initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 <title>Red House Expense Tracking System</title>
 <link type="text/css" rel="stylesheet" media="all"  href="<?=base_url("/css/main.css");?>" />
 <link type="text/css" rel="stylesheet"  media="only screen 
