4eb4aa1a79401e8435b31ec31791f22f5e3ae3ab
Chris Dart <chris@ten7.com>
2018-12-29 20:47:21 -0600

    Upgrade to CI 3.1.9

--- application/views/fee/list.php
+++ application/views/fee/list.php
@@ -42,43 +42,5 @@ $share_total = $user_count * 130;
         </tbody>
     </table>
 </div>
-<div id="budget" class="block border">
-    <h3>Budget Summary</h3>
-    <table class="list table">
-        <thead>
-        <tr>
-            <th>Budget</th>
-            <th>Actual</th>
-            <th>Diff</th>
-            <th>Percent</th>
-        </tr>
-        </thead>
-        <tr>
-            <td>Current Month</td>
-        </tr>
-        <tr>
-            <td><?php echo get_as_cash ($share_total);?></td>
-            <td><?php echo get_as_cash($expense_total); ?></td>
-            <td><?php echo get_as_cash($expense_total - $share_total); ?></td>
-            <td><?php echo round(($expense_total - $share_total) / $share_total * 100, 2); ?>%
-            </td>
-        </tr>
-        <tr>
-            <td colspan="4">
-                Year-to-Date
-            </td>
-        </tr>
-        <tr>
-            <?php $ytd_budget = $share_total * $month_count ?>
-            <td><?php echo get_as_cash($ytd_budget); ?></td>
-            <td><?php echo get_as_cash($ytd_expense_total); ?></td>
-            <td><?php echo get_as_cash($ytd_budget - $ytd_expense_total); ?></td>
-            <td><?php echo round(($ytd_expense_total - $ytd_budget) / $ytd_budget * 100, 2); ?>
-                %
-            </td>
-        </tr>
-    </table>
-
-</div>
 
 <!-- end fee/list.php -->
