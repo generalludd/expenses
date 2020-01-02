fd62a679e9d6b1fc3b5240e6990b4e8e2e9f1ec9
Chris Dart <chrisdart@darter.localdomain>
2020-01-01 18:20:45 -0600

    Prevent db logging on live unless with a uri query value of debug

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -10,9 +10,10 @@ class MY_Model extends CI_Model
 			parent::__construct ();
 		}
 
-		function _log ( $element = "alert" )
-		{
-			$last_query = $this->db->last_query ();
-				$this->session->set_flashdata ( $element, $last_query );
+		function _log ( $element = "alert" ) {
+			if ($this->config->item('base_url') == 'http://docker.test' || $this->input->get('debug')){
+				$last_query = $this->db->last_query();
+				$this->session->set_flashdata($element, $last_query);
+			}
 		}
 	}
