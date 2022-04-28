2ce32b42735927f0006d9b7235fea3817133f5f9
Emily <emily.susann@gmail.com>
2022-04-27 22:17:22 -0500

    Issue #24 add env functionality to email

--- src/application/config/email.php
+++ src/application/config/email.php
@@ -1,16 +1,10 @@
 <?php
-
-switch($_SERVER['HTTP_HOST']){
-	case 'docker.test':
-	case 'backoffice.test':
-	$config['protocol'] = 'smtp';
-	$config['smtp_host'] = 'mailhog';
-	$config['smtp_port'] = '1024';
-	$config['smtp_crypto'] = '';
-		$config['smtp_user'] = '';
-		$config['smtp_pass'] = '';
-		break;
-	default:
-
-		break;
-}
+$config["mailpath"] = getenv('EXP_MAILPATH');
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = getenv('EXP_EMAIL_HOST');
+$config['smtp_port'] = getenv('EXP_EMAIL_PORT');
+$config['smtp_crypto'] = '';
+$config['smtp_user'] = getenv('EXP_EMAIL_USER');
+$config['smtp_pass'] = getenv('EXP_EMAIL_PASS');
+$config["newline"] = "\r\n";
+$config["charset"] = "utf-8";
