4e87369f7eb89052e88939c3cd4e151f38bbcb16
Emily <emily.susann@gmail.com>
2022-04-27 21:52:27 -0500

    Issue #24 add env functionality to database

--- src/application/config/database.php
+++ src/application/config/database.php
@@ -4,22 +4,22 @@ $active_record = TRUE;
 
 
 
-if(empty(getenv('MYSQL_HOST'))){
-    exit('MYSQL_HOST was not defined');
+if(empty(getenv('EXP_HOST'))){
+    exit('EXP_HOST was not defined');
 }
-if(empty(getenv('MYSQL_USER'))){
-    exit('MYSQL_USER was not defined');
+if(empty(getenv('EXP_USER'))){
+    exit('EXP_USER was not defined');
 }
-if(empty(getenv('MYSQL_PASS'))){
-    exit('MYSQL_PASS not defined');
+if(empty(getenv('EXP_PASS'))){
+    exit('EXP_PASS not defined');
 }
-if(empty(getenv('MYSQL_DB'))){
-    exit('MYSQL_DB not defined');
+if(empty(getenv('EXP_DB'))){
+    exit('EXP_DB not defined');
 }
-$db['default']['hostname'] = getenv('MYSQL_HOST');
-$db['default']['username'] = getenv('MYSQL_USER');
-$db['default']['password'] = getenv('MYSQL_PASS');
-$db['default']['database'] = getenv('MYSQL_DB');
+$db['default']['hostname'] = getenv('EXP_HOST');
+$db['default']['username'] = getenv('EXP_USER');
+$db['default']['password'] = getenv('EXP_PASS');
+$db['default']['database'] = getenv('EXP_DB');
 
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
--- src/application/config/hooks.php
+++ src/application/config/hooks.php
@@ -1,6 +1,4 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /*
 | -------------------------------------------------------------------------
 | Hooks
@@ -8,6 +6,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | This file lets you define "hooks" to extend CI without hacking the core
 | files.  Please see the user guide for info:
 |
-|	https://codeigniter.com/user_guide/general/hooks.html
+|	http://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+// Load the envelope file for secrets if the file exists. 
+
+$hook['pre_system'] = function () {
+    $env_path = __DIR__.'/../../../.env';
+    if(file_exists($env_path)){
+        $dotenv = new Symfony\Component\Dotenv\Dotenv();
+        $dotenv->load($env_path);
+    }
+};
+
+
+/* End of file hooks.php */
+/* Location: ./application/config/hooks.php */
