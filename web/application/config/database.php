e45dfc43ae0a0330e663257803367ec9c3132c52
Chris Dart <571478+generalludd@users.noreply.github.com>
2023-12-16 16:25:38 -0600

    Add ddev

---
+++ web/application/config/database.php
@@ -0,0 +1,7 @@
+<?php
+
+if(file_exists(__DIR__ .'/database.local.php')){
+  require_once(__DIR__ . '/database.local.php');
+}
+
+
---
+++ web/application/config/email.php
@@ -0,0 +1,4 @@
+<?php
+if(file_exists(__DIR__ . '/email.local.php')){
+  require_once(__DIR__ . '/email.local.php');
+}
